BlueSplash/svn-desmume

commit 9b3061cecfef6717bc459e349b9d94b926037695

gfx3d: fix advance wars invisible tiles yet again.
git-svn-id: http://svn.code.sf.net/p/desmume/code/trunk@2436 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/gfx3d.cpp
@@ -1418,8 +1418,12 @@ static bool gfx3d_ysort_compare(int num1, int num2)
 	if (poly1.maxy < poly2.maxy) return true;
 	if (poly1.miny < poly2.miny) return true;
 	if (poly1.miny > poly2.miny) return false;
-	if (num1 < num2) return false; //make sure we respect the game's ordering in cases of complete ties
-	else return true;
+	
+	//make sure we respect the game's ordering in cases of complete ties
+	//this makes it a stable sort.
+	//this must be a stable sort or else advance wars DOR will flicker in the main map mode
+	if (num1 < num2) return true;
+	else return false;
 }
 
 static void gfx3d_doFlush()
@@ -1473,14 +1477,13 @@ static void gfx3d_doFlush()
 	//now we have to sort the opaque polys by y-value.
 	//(test case: harvest moon island of happiness character cretor UI)
 	//should this be done after clipping??
-	//this must be a stable sort or else advance wars DOR will flicker in the main map mode
-	std::stable_sort(gfx3d.indexlist, gfx3d.indexlist + opaqueCount, gfx3d_ysort_compare);
+	std::sort(gfx3d.indexlist, gfx3d.indexlist + opaqueCount, gfx3d_ysort_compare);
 	
 	if(!gfx3d.sortmode)
 	{
 		//if we are autosorting translucent polys, we need to do this also
 		//TODO - this is unverified behavior. need a test case
-		std::stable_sort(gfx3d.indexlist + opaqueCount, gfx3d.indexlist + polycount, gfx3d_ysort_compare);
+		std::sort(gfx3d.indexlist + opaqueCount, gfx3d.indexlist + polycount, gfx3d_ysort_compare);
 	}
 
 	//switch to the new lists

commit 3aa6334f29bb00f2fcf31ec68f30af785e02d6a2

GFX3D: - Fix possible invalid memory access crashes when Y-sorting, most notably, in Super Mario 64 adventure mode. Using std::stable_sort() instead of std::sort() should have little to no performance impact since we're not sorting a lot of elements here. (Regression from r2436.)
git-svn-id: http://svn.code.sf.net/p/desmume/code/trunk@5089 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/gfx3d.cpp
@@ -2095,8 +2095,7 @@ static bool gfx3d_ysort_compare(int num1, int num2)
 	//make sure we respect the game's ordering in cases of complete ties
 	//this makes it a stable sort.
 	//this must be a stable sort or else advance wars DOR will flicker in the main map mode
-	if (num1 < num2) return true;
-	else return false;
+	return (num1 < num2);
 }
 
 static void gfx3d_doFlush()
@@ -2187,13 +2186,13 @@ static void gfx3d_doFlush()
 	//now we have to sort the opaque polys by y-value.
 	//(test case: harvest moon island of happiness character cretor UI)
 	//should this be done after clipping??
-	std::sort(gfx3d.indexlist.list, gfx3d.indexlist.list + opaqueCount, gfx3d_ysort_compare);
+	std::stable_sort(gfx3d.indexlist.list, gfx3d.indexlist.list + opaqueCount, gfx3d_ysort_compare);
 	
 	if(!gfx3d.state.sortmode)
 	{
 		//if we are autosorting translucent polys, we need to do this also
 		//TODO - this is unverified behavior. need a test case
-		std::sort(gfx3d.indexlist.list + opaqueCount, gfx3d.indexlist.list + polycount, gfx3d_ysort_compare);
+		std::stable_sort(gfx3d.indexlist.list + opaqueCount, gfx3d.indexlist.list + polycount, gfx3d_ysort_compare);
 	}
 
 	//switch to the new lists