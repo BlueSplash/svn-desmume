BlueSplash/svn-desmume

commit 9205b3759c3afa4ca7779a8c6604bff5a5eee1b6

GPU: - Better organize BG layer states. - Do a bunch of other code cleanup.
git-svn-id: http://svn.code.sf.net/p/desmume/code/trunk@5286 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/GPU.h
@@ -910,9 +910,12 @@ enum SpriteRenderMode
 
 typedef struct
 {
-	 s16 x;
-	 s16 y;
-} SpriteSize;
+	u16 width;
+	u16 height;
+} GPUSize_u16;
+
+typedef GPUSize_u16 SpriteSize;
+typedef GPUSize_u16 BGLayerSize;
 
 typedef u8 TBlendTable[32][32];
 
@@ -948,8 +951,7 @@ enum GPULayerID
 	GPULayerID_BG2					= 2,
 	GPULayerID_BG3					= 3,
 	GPULayerID_OBJ					= 4,
-	
-	GPULayerID_None					= 5
+	GPULayerID_Backdrop				= 5
 };
 
 enum BGType
@@ -1046,6 +1048,28 @@ typedef struct
 
 #define VRAM_NO_3D_USAGE 0xFF
 
+typedef struct
+{
+	GPULayerID layerID;
+	IOREG_BGnCNT BGnCNT;
+	
+	BGLayerSize size;
+	BGType type;
+	u8 priority;
+	
+	bool isVisible;
+	bool isMosaic;
+	bool isDisplayWrapped;
+	
+	u8 extPaletteSlot;
+	u16 *extPalette;
+	
+	u32 largeBMPAddress;
+	u32 BMPAddress;
+	u32 tileMapAddress;
+	u32 tileEntryAddress;
+} BGLayerInfo;
+
 class GPUEngineBase
 {
 protected:
@@ -1054,8 +1078,8 @@ protected:
 	static CACHE_ALIGN u8 _blendTable555[17][17][32][32];
 	
 	static const CACHE_ALIGN SpriteSize _sprSizeTab[4][4];
+	static const CACHE_ALIGN BGLayerSize _BGLayerSizeLUT[8][4];
 	static const CACHE_ALIGN BGType _mode2type[8][4];
-	static const CACHE_ALIGN u16 _sizeTab[8][4][2];
 	static const CACHE_ALIGN u8 _winEmpty[GPU_FRAMEBUFFER_NATIVE_WIDTH];
 	
 	static struct MosaicLookup {
@@ -1082,7 +1106,7 @@ protected:
 	CACHE_ALIGN u8 _sprWin[GPU_FRAMEBUFFER_NATIVE_WIDTH];
 	
 	bool _enableLayer[5];
-	bool _isBGLayerEnabled;
+	bool _isAnyBGLayerEnabled;
 	itemsForPriority_t _itemsForPriority[NB_PRIORITIES];
 	
 	struct MosaicColor {
@@ -1101,23 +1125,16 @@ protected:
 	OAMAttributes *_oamList;
 	u32 _sprMem;
 	
-	u8 _bgPrio[5];
-	bool _bg0HasHighestPrio;
-	
 	u8 _sprBoundary;
 	u8 _sprBMPBoundary;
 	
-	bool _blend2[8];
+	bool _blend2[6];
 	
 	TBlendTable *_blendTable;
 	u16 *_currentFadeInColors;
 	u16 *_currentFadeOutColors;
 	
-	u32 _BG_bmp_large_ram[4];
-	u32 _BG_bmp_ram[4];
-	u32 _BG_tile_ram[4];
-	u32 _BG_map_ram[4];
-	BGType _BGTypes[4];
+	BGLayerInfo _BGLayer[4];
 	
 	CACHE_ALIGN u8 _sprNum[256];
 	CACHE_ALIGN u8 _h_win[2][GPU_FRAMEBUFFER_NATIVE_WIDTH];
@@ -1146,6 +1163,7 @@ protected:
 	
 	void _InitLUTs();
 	void _Reset_Base();
+	void _ResortBGLayers();
 	
 	void _MosaicSpriteLinePixel(const size_t x, u16 l, u16 *dst, u8 *dst_alpha, u8 *typeTab, u8 *prioTab);
 	void _MosaicSpriteLine(u16 l, u16 *dst, u8 *dst_alpha, u8 *typeTab, u8 *prioTab);
@@ -1153,7 +1171,6 @@ protected:
 	template<rot_fun fun, bool WRAP> void _rot_scale_op(u16 *dstColorLine, const u16 lineIndex, const IOREG_BGnParameter &param, const u16 LG, const s32 wh, const s32 ht, const u32 map, const u32 tile, const u16 *pal);
 	template<GPULayerID LAYERID, rot_fun fun> void _apply_rot_fun(u16 *dstColorLine, const u16 lineIndex, const IOREG_BGnParameter &param, const u16 LG, const u32 map, const u32 tile, const u16 *pal);
 	
-	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED> void _LineLarge8bpp(u16 *dstColorLine, const u16 lineIndex);
 	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED> void _RenderLine_TextBG(u16 *dstColorLine, const u16 lineIndex, u16 XBG, u16 YBG, u16 LG);
 	
 	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED> void _RotBG2(u16 *dstColorLine, const u16 lineIndex, const IOREG_BGnParameter &param, const u16 LG);
@@ -1196,7 +1213,6 @@ public:
 	virtual ~GPUEngineBase();
 	
 	virtual void Reset();
-	void ResortBGLayers();
 	void SetupFinalPixelBlitter();
 	void RefreshAffineStartRegs();
 	
@@ -1215,9 +1231,6 @@ public:
 	
 	template<bool ISCUSTOMRENDERINGNEEDED> void RenderLine(const u16 l, bool isFrameSkipRequested);
 	void FramebufferPostprocess();
-	
-	u16 BGSize[4][2];
-	u8 BGExtPalSlot[4];
 	
 	bool isCustomRenderingNeeded;
 	bool is3DEnabled;
@@ -1250,6 +1263,8 @@ public:
 	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool ISCUSTOMRENDERINGNEEDED, bool USECUSTOMVRAM> FORCEINLINE void ____setFinalColorBck(u16 *dstColorLine, const u16 lineIndex, const u16 color, const size_t srcX);
 	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED, bool USECUSTOMVRAM> FORCEINLINE void ___setFinalColorBck(u16 *dstColorLine, const u16 lineIndex, u16 color, const size_t srcX, const bool opaque);
 	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED> FORCEINLINE void __setFinalColorBck(u16 *dstColorLine, const u16 lineIndex, u16 color, const size_t srcX, const bool opaque);
+	
+	const BGLayerInfo& GetBGLayerInfoByID(const GPULayerID layerID);
 	
 	void UpdateVRAM3DUsageProperties_BGLayer(const size_t bankIndex, VRAM3DUsageProperties &outProperty);
 	void UpdateVRAM3DUsageProperties_OBJLayer(const size_t bankIndex, VRAM3DUsageProperties &outProperty);
@@ -1319,6 +1334,8 @@ public:
 	FragmentColor* Get3DFramebufferRGBA6665() const;
 	u16* Get3DFramebufferRGBA5551() const;
 	virtual void SetCustomFramebufferSize(size_t w, size_t h);
+	
+	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED> void _LineLarge8bpp(u16 *dstColorLine, const u16 lineIndex);
 		
 	template<bool ISCUSTOMRENDERINGNEEDED> void RenderLine(const u16 l, bool isFrameSkipRequested);
 	void FramebufferPostprocess();

desmume/src/windows/mapView.cpp
@@ -70,10 +70,10 @@ LRESULT MapView_OnPaint(mapview_struct * win, HWND hwnd, WPARAM wParam, LPARAM l
 	PAINTSTRUCT  ps;
 	char text[80];
 	u32 dispcnt = ((volatile u32 *)MMU.ARM9_REG)[(win->engineID*0x400)];
-	u32 bgcnt =  ((volatile u16 *)MMU.ARM9_REG)[(8 + (win->layerID<<1) + (win->engineID*0x1000))>>1];
+	const BGLayerInfo &BGLayer = (win->engineID == GPUEngineID_Main) ? GPU->GetEngineMain()->GetBGLayerInfoByID(win->layerID) : GPU->GetEngineSub()->GetBGLayerInfoByID(win->layerID);
 	BITMAPV4HEADER bmi;
-	u16 lg;
-	u16 ht;
+	u16 lg = BGLayer.size.width;
+	u16 ht = BGLayer.size.height;
 	//BGxPARMS * parms;
 
 	//CreateBitmapIndirect(&bmi);
@@ -85,26 +85,15 @@ LRESULT MapView_OnPaint(mapview_struct * win, HWND hwnd, WPARAM wParam, LPARAM l
 	bmi.bV4RedMask = 0x001F;
 	bmi.bV4GreenMask = 0x03E0;
 	bmi.bV4BlueMask = 0x7C00;
-
-	if(win->engineID == GPUEngineID_Main)
-	{
-		lg = GPU->GetEngineMain()->BGSize[win->layerID][0];
-		ht = GPU->GetEngineMain()->BGSize[win->layerID][1];
-	}
-	else
-	{
-		lg = GPU->GetEngineSub()->BGSize[win->layerID][0];
-		ht = GPU->GetEngineSub()->BGSize[win->layerID][1];
-	}
 	bmi.bV4Width = lg;
 	bmi.bV4Height = -ht;
 
 	hdc = BeginPaint(hwnd, &ps);        
 
-	sprintf(text, "%d %08X, %08X", (int)(dispcnt&7), (int)dispcnt, (int)bgcnt);
+	sprintf(text, "%d %08X, %08X", (int)(dispcnt&7), (int)dispcnt, (int)BGLayer.BGnCNT.value);
 	SetWindowText(GetDlgItem(hwnd, IDC_MODE), text);
 
-	if(!(bgcnt&(1<<7)))
+	if(BGLayer.BGnCNT.PaletteMode == 0)
 		sprintf(text, "normal 16");
 	else
 	{
@@ -112,39 +101,27 @@ LRESULT MapView_OnPaint(mapview_struct * win, HWND hwnd, WPARAM wParam, LPARAM l
 			sprintf(text, "normal 256");
 		else
 		{
-			switch(win->layerID)
-			{
-			case GPULayerID_BG0 :
-				sprintf(text, "extended slot %d", (bgcnt&(1<<13))?2:0);
-				break;
-			case GPULayerID_BG1 :
-				sprintf(text, "extended slot %d", (bgcnt&(1<<13))?3:1);
-				break;
-			default :
-				sprintf(text, "extended slot %d", GPU->GetEngineMain()->BGExtPalSlot[win->layerID]);
-				break;
-			}
+			sprintf(text, "extended slot %d", BGLayer.extPaletteSlot);
 		}
 	}
 	SetWindowText(GetDlgItem(hwnd, IDC_PAL), text);
 
-	sprintf(text, "%d", (int)(bgcnt&3));
+	sprintf(text, "%d", (int)(BGLayer.priority));
 	SetWindowText(GetDlgItem(hwnd, IDC_PRIO), text);
 
-
-	if((dispcnt>>8>>win->layerID)&1)
+	if(BGLayer.isVisible)
 		SetWindowText(GetDlgItem(hwnd, IDC_VISIBLE), "true");
 	else
 		SetWindowText(GetDlgItem(hwnd, IDC_VISIBLE), "false");
 
-	sprintf(text, "0x%08X", (int)(0x6000000 + ((bgcnt>>2)&0xF)*0x4000 + win->engineID*0x200000 +((dispcnt>>24)&7)*0x10000));
+	sprintf(text, "0x%08X", (int)(0x6000000 + (BGLayer.BGnCNT.CharacBase_Block)*0x4000 + win->engineID*0x200000 +((dispcnt>>24)&7)*0x10000));
 	SetWindowText(GetDlgItem(hwnd, IDC_CHAR), text);
 
-	sprintf(text, "0x%08X", (int)(0x6000000 + 0x800*((bgcnt>>8)&0x1F) + win->engineID*0x200000 + ((dispcnt>>27)&7)*0x10000));
+	sprintf(text, "0x%08X", (int)(0x6000000 + 0x800*(BGLayer.BGnCNT.ScreenBase_Block) + win->engineID*0x200000 + ((dispcnt>>27)&7)*0x10000));
 	SetWindowText(GetDlgItem(hwnd, IDC_SCR), text);
 
 	//sprintf(text, "%d x %d",  GPU->GetEngineMain()->BGPA[win->map], GPU->GetEngineMain()->BGPB[win->map]);
-	sprintf(text, "%d x %d",  (int)GPU->GetEngineMain()->BGSize[win->layerID][0], (int)GPU->GetEngineMain()->BGSize[win->layerID][1]);
+	sprintf(text, "%d x %d",  (int)lg, (int)ht);
 	SetWindowText(GetDlgItem(hwnd, IDC_MSIZE), text);
 
 	//if (win->map==2) {

commit 3040d73724e236797d49669e2afabe6aac714e46

GPU: - Account for the fact that extended palette mappings can change independently of the BGnCNT register. Fixes the BG3 layer in Phoenix Wright: Ace Attorney. (Regression from r5286.)
git-svn-id: http://svn.code.sf.net/p/desmume/code/trunk@5296 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/GPU.cpp
@@ -396,10 +396,10 @@ void GPUEngineBase::_Reset_Base()
 	
 	this->_BGLayer[GPULayerID_BG0].extPaletteSlot = GPULayerID_BG0;
 	this->_BGLayer[GPULayerID_BG1].extPaletteSlot = GPULayerID_BG1;
-	this->_BGLayer[GPULayerID_BG0].extPalette = (u16 *)MMU.ExtPal[this->_engineID][GPULayerID_BG0];
-	this->_BGLayer[GPULayerID_BG1].extPalette = (u16 *)MMU.ExtPal[this->_engineID][GPULayerID_BG1];
-	this->_BGLayer[GPULayerID_BG2].extPalette = (u16 *)MMU.ExtPal[this->_engineID][GPULayerID_BG2];
-	this->_BGLayer[GPULayerID_BG3].extPalette = (u16 *)MMU.ExtPal[this->_engineID][GPULayerID_BG3];
+	this->_BGLayer[GPULayerID_BG0].extPalette = (u16 **)&MMU.ExtPal[this->_engineID][GPULayerID_BG0];
+	this->_BGLayer[GPULayerID_BG1].extPalette = (u16 **)&MMU.ExtPal[this->_engineID][GPULayerID_BG1];
+	this->_BGLayer[GPULayerID_BG2].extPalette = (u16 **)&MMU.ExtPal[this->_engineID][GPULayerID_BG2];
+	this->_BGLayer[GPULayerID_BG3].extPalette = (u16 **)&MMU.ExtPal[this->_engineID][GPULayerID_BG3];
 	
 	this->_curr_win[0] = GPUEngineBase::_winEmpty;
 	this->_curr_win[1] = GPUEngineBase::_winEmpty;
@@ -644,7 +644,7 @@ void GPUEngineBase::ParseReg_BGnCNT()
 	this->_BGLayer[LAYERID].size = GPUEngineBase::_BGLayerSizeLUT[mode][BGnCNT.ScreenSize];
 	this->_BGLayer[LAYERID].isMosaic = (BGnCNT.Mosaic != 0);
 	this->_BGLayer[LAYERID].priority = BGnCNT.Priority;
-	this->_BGLayer[LAYERID].extPalette = (u16 *)MMU.ExtPal[this->_engineID][this->_BGLayer[LAYERID].extPaletteSlot];
+	this->_BGLayer[LAYERID].extPalette = (u16 **)&MMU.ExtPal[this->_engineID][this->_BGLayer[LAYERID].extPaletteSlot];
 	
 	this->_ResortBGLayers();
 }
@@ -1397,7 +1397,7 @@ void GPUEngineBase::_RenderLine_TextBG(u16 *dstColorLine, const u16 lineIndex, u
 	}
 	else //256-color BG
 	{
-		const u16 *pal = (DISPCNT.ExBGxPalette_Enable) ? this->_BGLayer[LAYERID].extPalette : this->_paletteBG;
+		const u16 *pal = (DISPCNT.ExBGxPalette_Enable) ? *(this->_BGLayer[LAYERID].extPalette) : this->_paletteBG;
 		
 		yoff = ((YBG&7)<<3);
 		xfin = 8 - (xoff&7);
@@ -1457,7 +1457,7 @@ void GPUEngineBase::_ExtRotBG2(u16 *dstColorLine, const u16 lineIndex, const IOR
 		{
 			if (DISPCNT.ExBGxPalette_Enable)
 			{
-				pal = this->_BGLayer[LAYERID].extPalette;
+				pal = *(this->_BGLayer[LAYERID].extPalette);
 				this->_apply_rot_fun< LAYERID, rot_tiled_16bit_entry<LAYERID, ISDEBUGRENDER, MOSAIC, true, ISCUSTOMRENDERINGNEEDED> >(dstColorLine, lineIndex, param, LG, this->_BGLayer[LAYERID].tileMapAddress, this->_BGLayer[LAYERID].tileEntryAddress, pal);
 			}
 			else

desmume/src/GPU.h
@@ -1079,7 +1079,7 @@ typedef struct
 	bool isDisplayWrapped;
 	
 	u8 extPaletteSlot;
-	u16 *extPalette;
+	u16 **extPalette;
 	
 	u32 largeBMPAddress;
 	u32 BMPAddress;